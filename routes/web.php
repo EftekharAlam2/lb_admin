fcf750e5aac81232da31a378986324ef7f032c7c
EftekharAlam2 <hinosuvlmare@gmail.com>
2024-01-22 15:47:11 +0600

    admin template add

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\frontend\IndexController;
+use App\Http\Controllers\frontend\AccountsController;
+use App\Http\Controllers\frontend\AddProductController;
+use App\Http\Controllers\frontend\EditProductController;
+use App\Http\Controllers\frontend\LoginController;
+use App\Http\Controllers\frontend\ProductsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +19,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/',[IndexController::class, 'index']);
+Route::get('/accounts',[AccountsController::class, 'index']);
+Route::get('/add-product',[AddProductController::class, 'index']);
+Route::get('/edit-product',[EditProductController::class, 'index']);
+Route::get('/login',[LoginController::class, 'index']);
+Route::get('/products',[ProductsController::class, 'index']);
